15861a45edb9137c08a9babc76ee42ba628cdd1e
Victor <vicpbarnes17@gmail.com>
2024-02-06 09:41:39 -0500

    First

---
+++ MovieLab/Movies.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLab
+{
+    internal class Movies
+    {
+        //properties
+        public string Title { get; set; }
+        public string Category { get; set; }
+        //constructor
+        public Movies(string _title, string _category)
+        {
+            Title = _title;
+            Category = _category;
+        }
+        //Methods
+        public virtual string TitleName (string name)
+        {
+            Title = name;
+            return Title;
+        }
+        public virtual string CategoryName (string name2)
+        {
+            Category = name2;
+            return Category;
+        }
+    }
+}
---
+++ MovieLab/Program.cs
@@ -0,0 +1,35 @@
+﻿using MovieLab;
+//using Validator;
+
+List<Movies> allMovies = new List<Movies>()
+{
+    new Movies ("How to trian your Dragon", "Fantasy"),
+    new Movies ("Pokemon" , "Anime"),
+    new Movies ("Spider-Man", "Fantasy"),
+    new Movies ("Inception", "Sci-Fi"),
+    new Movies ("The Dark Knight", "Action"),
+    new Movies ("The Matrix", "Sci-Fi"),
+    new Movies ("Titanic", "Romance"),
+    new Movies ("Cinderella", "Romance"),
+    new Movies ("Hacksaw Ridge", "War"),
+    new Movies ("Forest Gump", "AutoBiography")
+
+};
+
+bool runProgram = true;
+while (runProgram)
+{
+
+    Console.WriteLine("Please choose a movie genre.");
+    string category = Console.ReadLine().ToLower().Trim();
+
+    List<Movies> choice = allMovies.Where(m => m.Category.ToLower().Trim() == category).ToList();
+
+    foreach (Movies m in choice)
+    {
+        Console.WriteLine($"{m.Title} {m.Category}");
+    }
+
+    runProgram = Validator.GetContinue();
+
+}
